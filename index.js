96346a6ff7a117b0677d7a1c4f8d174971c258a2
leozhou <492753662@qq.com>
2016-03-20 17:02:35 +0800

    update route

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var app = require('express')();
 
+function authorize(req, res ,next){
+	if (req.session.authorized) {return next();};
+	res.render('not-authorized');
+};
+app.get('/secret',authorize, function(){
+	res.render('secret');
+});
+app.get('/sub-rosa',authorize, function(){
+	res.render('sub-rosa');
+});
 
 app.get('/foo', function(req, res, next){
 	if (Math.random() < 0.5) {next()};
@@ -11,7 +21,7 @@ app.get('/foo', function(req, res){
 	res.send('and sometimes that');
 });
 
-app.get('/eoo', function(req, res){
+app.get('/eoo', function(req, res, next){
 	if (Math.random() < 0.33) {return next();};
 	res.send('red');
 },
@@ -21,6 +31,27 @@ function(req, res, next){
 },
 function(req, res){
 	res.send('blue');
+});
+
+//路由参数
+var staff = {
+	portland:{
+		mitch: {bio: 'Mitch is the man to have at your back in a bar fight.'},
+		madeline: {bio: 'Madeline is out Oregon expert.'},
+	},
+	bend:{
+		walt:{bio: 'Walt is our Oregon Coast expert.'},
+	},
+};
+
+app.get('/staff/:city/:name',function(req, res, next){
+	var info = staff[req.params.city][req.params.name];
+	if (!info) {return next();};
+	res.send('staffer',info);
+});
+
+app.use(function(req, res){
+	res.send("404");
 });
 
 app.listen(3000, function(){
